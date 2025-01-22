8445fa1b269d738d9d78295eaaca06b45aeee1c0
Banjo <xxxx@gmail.com>
2025-01-22 22:41:08 +0000

    update

--- search-tutorial/search.py
+++ search-tutorial/search.py
@@ -11,63 +11,55 @@ class Search:
     def __init__(self):
         self.es = Elasticsearch(cloud_id=os.environ['ELASTIC_CLOUD_ID'],
                               api_key=os.environ['ELASTIC_API_KEY'])
+        self.search_index = 'search-dev-spark-product-index'
+        self.autocomplete_index = 'search-dev-spark-product-autocomplete'
         client_info = self.es.info()
         print('Connected to Elasticsearch!')
         pprint(client_info.body)
 
     def search(self, **query_args):
-        return self.es.search(index='search-dev-spark-product-index', **query_args)
+        return self.es.search(index=self.search_index, **query_args)
 
     def get_suggestions(self, query_text):
         try:
-            # Using completion suggester
+            # Search in the autocomplete index first
             suggest_query = {
-                'suggest': {
-                    'product-suggester': {
-                        'prefix': query_text,
-                        'completion': {
-                            'field': 'name.suggest',
-                            'fuzzy': {
-                                'fuzziness': 1
-                            },
-                            'size': 10
-                        }
+                'query': {
+                    'prefix': {
+                        'name': query_text
                     }
-                }
+                },
+                'size': 10
             }
             
             response = self.es.search(
-                index='search-dev-spark-product-index',
+                index=self.autocomplete_index,
                 body=suggest_query
             )
             
             suggestions = []
-            if 'suggest' in response:
-                for suggestion in response['suggest']['product-suggester'][0]['options']:
-                    suggestions.append({
-                        'value': suggestion['text'],
-                        'label': suggestion['text']
-                    })
+            for hit in response['hits']['hits']:
+                suggestions.append({
+                    'value': hit['_source']['name'],
+                    'label': hit['_source']['name']
+                })
             
-            # If completion suggester returns no results, fallback to search-as-you-type
+            # If no suggestions found, try the main search index
             if not suggestions:
                 search_query = {
                     'query': {
-                        'multi_match': {
-                            'query': query_text,
-                            'type': 'bool_prefix',
-                            'fields': [
-                                'name',
-                                'name._2gram',
-                                'name._3gram'
-                            ]
+                        'match_phrase_prefix': {
+                            'name': {
+                                'query': query_text,
+                                'max_expansions': 10
+                            }
                         }
                     },
                     'size': 10
                 }
                 
                 response = self.es.search(
-                    index='search-dev-spark-product-index',
+                    index=self.search_index,
                     body=search_query
                 )
                 
